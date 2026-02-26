edad8064d40156a739ed4933793eb16ee1eb7fad
Sonya <preminina.sofya@gmail.com>
2026-02-26 23:53:18 +0300

    feat: create Slide model with filer image field

--- slider/models.py
+++ slider/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from filer.fields.image import FilerImageField
 
-# Create your models here.
+class Slide(models.Model):
+    title = models.CharField("Название", max_length=200)
+    image = FilerImageField(
+        on_delete=models.CASCADE,
+        verbose_name="Изображение",
+        related_name="slides",
+    )
+    order = models.PositiveIntegerField("Порядок", default=0)
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = "Слайд"
+        verbose_name_plural = "Слайды"
+
+    def __str__(self):
+        return self.title
