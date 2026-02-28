dd4ab035b90e1b2b50636dcf44ad3ea79394cdb2
Sonya <preminina.sofya@gmail.com>
2026-03-01 02:09:03 +0300

    feat: add fullscreen gallery modal with slick slider

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -18,7 +18,48 @@ $(document).ready(function(){
         centerPadding: '0px',
         focusOnSelect: true,
         infinite: true,
-        arrows: true
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                centerMode: true,
+                centerPadding: '30px'
+            }
+            }
+        ]
+    });
+
+    $('.slider-for').on('click', 'img', function () {
+
+        let index = $(this).closest('.slick-slide').data('slick-index');
+
+        let modal = new bootstrap.Modal(document.getElementById('galleryModal'));
+        modal.show();
+
+        $('#galleryModal').one('shown.bs.modal', function () {
+
+            if (!$('.modal-slider').hasClass('slick-initialized')) {
+                $('.modal-slider').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    infinite: true,
+                    speed: 500,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                    pauseOnHover: false,
+                    pauseOnFocus: false,
+                });
+            }
+
+            $('.modal-slider').slick('setPosition');
+            $('.modal-slider').slick('slickGoTo', index, true);
+
+        });
     });
 
 });
